2137ba47fd00b77357ee8cdf507738088fb0f29d
이경일 <dlruddlf07@gmail.com>
2024-03-06 15:23:39 +0900

    feat: WEB-4455 함수형 컴포넌트 화살표함수 rule 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,14 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'import/no-unresolved': 'off', // vite.config '@' alias 설정을 인지못하는 이슈로 추가
+    'import/extensions': 'off', // import문 작성 시 확장자없이 사용
+    'react/function-component-definition': [
+      'warn',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
     'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }], // .tsx 확장자에서만 jsx 문법을 사용하도록 제한
   },
 };
