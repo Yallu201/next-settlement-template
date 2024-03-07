f6b56d7ea84ca00797bf2da82e8b2f61a11d9651
이경일 <dlruddlf07@gmail.com>
2024-03-07 15:36:35 +0900

    feat: WEB-4455 env 환경변수 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   cacheDir: './.vite',
+  envDir: './.env',
   resolve: {
     alias: {
       '@': '/src',
