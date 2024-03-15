e886123a5ee387575ce81091bb5970fb3ef8e724
이경일 <dlruddlf07@gmail.com>
2024-03-15 09:54:54 +0900

    feat: WEB-4455 utils/string.ts 내 lodash 사용부분을  vanila js로 대체

--- src/common/utils/string.ts
+++ src/common/utils/string.ts
@@ -1,11 +1,10 @@
-import _ from 'lodash';
-
-export const setEmptyOrStr = (v) => {
-  if (_.isString(v) && _.isEmpty(v)) return undefined;
-  else return v;
+export const setEmptyOrStr = (v: unknown) => {
+  if (typeof v === 'string' && v.trim() === '') return undefined;
+  return v;
 };
 
-export const setBooleanStr = (v) => {
+export const setBooleanStr = (v: unknown) => {
   if (v === true || v === 'true') return true;
   if (v === false || v === 'false') return false;
+  return false;
 };
