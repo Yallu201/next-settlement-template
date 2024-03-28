74a21992681bafc3e62391cc3da3fb6153844ec5
이경일 <sudal@deali.net>
2024-03-28 09:43:39 +0900

    build: WEB-4455 템플릿에 불필요한 API 도메인 환경변수 제거

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,10 +1,4 @@
 export const APP_ENV = import.meta.env.MODE;
-export const ADMIN_API_BASE_URL = import.meta.env.VITE_ADMIN_API_BASE_URL;
-export const BILLING_API_BASE_URL = import.meta.env.VITE_BILLING_API_BASE_URL;
-export const DDINGDONG_API_BASE_URL = import.meta.env.VITE_DDINGDONG_API_BASE_URL;
-export const ONDO_API_BASE_URL = import.meta.env.VITE_ONDO_API_BASE_URL;
-
-export const IMAGE_RESIZER_BASE_URL = import.meta.env.VITE_IMAGE_RESIZER_BASE_URL;
 
 export const AG_GRID_KEY = import.meta.env.VITE_AG_GRID_KEY;
 
