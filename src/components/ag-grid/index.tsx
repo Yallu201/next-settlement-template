34e00047ca9ef56104e6cbba6a32e4a824b981ed
이경일 <sudal@deali.net>
2024-03-28 16:27:08 +0900

    feat: WEB-4455 AgGrid 신규 컴포넌트 간단한 주석 추가

--- src/components/ag-grid/index.tsx
+++ src/components/ag-grid/index.tsx
@@ -3,6 +3,9 @@ import { forwardRef } from 'react';
 import clsx from 'clsx';
 import AgGridStyle from '@/styles/emotion/ag-grid';
 
+/**
+ * alpine 테마를 기본으로 적용한 AgGridReact 컴포넌트 입니다.
+ */
 const AgGrid = forwardRef<AgGridReact, AgGridReactProps>((props, ref) => {
   const globalClasses = AgGridStyle();
   return <AgGridReact ref={ref} className={clsx(globalClasses.agGridContainer, 'ag-theme-alpine')} {...props} />;
